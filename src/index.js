23732fe353398181a538a3981e07585c752be2e8
Morklympious <34610940+Morklympious@users.noreply.github.com>
2019-05-10 09:34:31 -0700

    [UI] Grid Layout for Attack Slots

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import Attack from "./components/attack.html";
 import Stance from "./components/stance-indicator.html";
+import Layout from "./layout.html";
 
-new Attack({
-    target : document.body,
+// new Attack({
+//     target : document.body,
+// });
+
+// new Stance({
+//     target : document.body,
+// })
+
+new Layout({
+    target : document.body
 });
-
-new Stance({
-    target : document.body,
-})
